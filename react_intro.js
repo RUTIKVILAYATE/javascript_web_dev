4cec07a479e84841c02ce5c2a6677a3839376f83
RUTIKVILAYATE <rutik.vilayate.ai@gmail.com>
2024-12-10 14:12:08 +0530

    React Intro

---
+++ react_intro.js
@@ -0,0 +1,5 @@
+// React Introduction
+// libraray made by google in 2011 and rerelease in 2013
+// open source, front-end, javascript library for building user interfaces and UI components
+// In react we can make separete component/classes to use it separately
+// that classes include state - data(uske andar kya hoga) about the component and methods(ke woh karta kya hai) 
